11ea4d7f6a207f89da1468635012183807df9d3a
Gustavo Varallo <gustavo.varallo@live.com>
2014-11-15 16:50:41 -0300

    Added categories and views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115192009) do
+ActiveRecord::Schema.define(version: 20141115192915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141115192009) do
     t.datetime "updated_at"
     t.string   "small_cover_url"
     t.string   "large_cover_url"
+    t.integer  "category_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@
 Video.create(title: "Monk", description: "Monk movie", large_cover_url: "/tmp/monk_large.jpg", small_cover_url: "/tmp/monk.jpg")
 Video.create(title: "Family Guy", description: "Family guy moive", small_cover_url: "/tmp/family_guy.jpg")
 Video.create(title: "Futurama", description: "Futurama movie", small_cover_url: "/tmp/futurama.jpg")
+Category.create(name: "Comedy")
+Category.create(name: "Drama")
+Category.create(name: "Suspense")
+Category.create(name: "Action")
