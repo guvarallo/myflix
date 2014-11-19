e867342148cde7627ad4b74405c7123dfc5f4a7c
Gustavo Varallo <gustavo.varallo@live.com>
2014-11-19 15:09:14 -0300

    Added search_by_title and category display change

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,5 +3,24 @@ require 'spec_helper'
 describe Category do
 
   it { should have_many(:videos) }
+
+  describe "#recent_videos" do
+    it "should display up to 6 videos by created_at DESC" do
+      c = Category.create(name: "Comedy")
+      v1 = Video.create(title: "Monk", description: "Monk movie 1", category: c)
+      v2 = Video.create(title: "Monk", description: "Monk movie 2", category: c)
+      v3 = Video.create(title: "Monk", description: "Monk movie 3", category: c)
+      v4 = Video.create(title: "Monk", description: "Monk movie 4", category: c)
+      v5 = Video.create(title: "Monk", description: "Monk movie 5", category: c)
+      v6 = Video.create(title: "Monk", description: "Monk movie 6", category: c)
+      v7 = Video.create(title: "Monk", description: "Monk movie 7", category: c)
+      expect(c.recent_videos).to eq([v7, v6, v5, v4, v3, v2])
+    end
+
+    it "should return an empty array if category has no videos" do
+      
+    end
+
+  end
   
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,37 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
+  describe 'search_by_title' do
+
+    it "should return an empty array if there's no match" do
+      Video.create(title: "Monk", description: "Monk movie")
+      expect(Video.search_by_title("Pulp Fiction")).to eq([])
+    end
+
+    it "should return all videos with exact match" do
+      Video.create(title: "Monk", description: "Monk movie")
+      Video.create(title: "Monk", description: "Monk movie second one")
+      Video.create(title: "Mo", description: "Monk movie second one")
+      expect(Video.search_by_title("Monk").size).to eq(2)
+    end
+    
+    it "should return all videos with partial match" do
+      video = Video.create(title: "Monk", description: "Monk movie")
+      expect(Video.search_by_title("Mo")).to eq([video])
+    end
+
+    it "should return all videos ordered by created_at ASC" do
+      video1 = Video.create(title: "Monk", description: "Monk movie")
+      video2 = Video.create(title: "Monk", description: "Monk movie second one")
+      expect(Video.search_by_title("Monk").order(created_at: :asc)).to eq([video1, video2])
+    end
+
+    it "should return an empty array if passed an empty string" do
+      video1 = Video.create(title: "Monk", description: "Monk movie")
+      video2 = Video.create(title: "Monk", description: "Monk movie second one")
+      expect(Video.search_by_title("")).to eq([])
+    end
+
+  end
+
 end
