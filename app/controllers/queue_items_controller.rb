09a614309743d43ce7d4ae083a7e55cc8e35b442
Gustavo Varallo <gustavo.varallo@live.com>
2014-12-19 18:41:49 -0300

    Added Queue Items

---
+++ app/controllers/queue_items_controller.rb
@@ -0,0 +1,25 @@
+class QueueItemsController < ApplicationController
+  before_action :require_user
+
+  def index
+    @queue_items = current_user.queue_items
+  end
+
+  def create
+    video = Video.find(params[:video_id])
+    QueueItem.create(video: video, user: current_user, 
+                     position: item_position) unless video_in_queue?(video)
+    redirect_to queue_path
+  end
+
+  private
+
+  def item_position
+    current_user.queue_items.count + 1
+  end
+
+  def video_in_queue?(video)
+    current_user.queue_items.map(&:video).include?(video)
+  end
+
+end
