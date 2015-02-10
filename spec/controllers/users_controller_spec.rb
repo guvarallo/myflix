4dd7a289ae19cafda51f0763adc8451c5b41f6aa
Gustavo Varallo <gustavo.varallo@live.com>
2015-02-10 12:52:11 -0300

    Added mailer tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,18 +3,15 @@ require 'spec_helper'
 describe UsersController do
 
   describe 'GET new' do
-
     it "should set @user" do
       get :new
       expect(assigns(:user)).to be_instance_of(User)
     end
-
   end
 
   describe 'POST create' do
     context "with valid input" do
       before { post :create, user: Fabricate.attributes_for(:user) }
-
       it "should create the user" do
         expect(User.count).to eq(1)
       end
@@ -24,9 +21,25 @@ describe UsersController do
       end
     end
 
+    context "send email" do
+      before { post :create, user: Fabricate.attributes_for(:user) }
+      it "sends the email" do
+        ActionMailer::Base.deliveries.should_not be_empty
+      end
+
+      it "sends to the right recepient" do
+        message = ActionMailer::Base.deliveries.last
+        message.to.should == [current_user.email]
+      end
+
+      it "has the right content" do
+        message = ActionMailer::Base.deliveries.last
+        message.encoded.should include(current_user.name)
+      end
+    end
+
     context "with invalid input" do
       before { post :create, user: { email: "abc@abc.com", password: "abc" } }
-
       it "should not create the user" do
         expect(User.count).to eq(0)
       end
@@ -38,7 +51,6 @@ describe UsersController do
       it "should set @user" do
         expect(assigns(:user)).to be_instance_of(User)
       end
-
     end
   end
 
